40cfc7a3d698a0d4dbfb0b9f7818c1db771b450d
Garrett Haufschild <garretthaufschild@gmail.com>
2017-10-21 21:14:14 -0400

    Added a whole load of shit

--- Web/script2.php
+++ Web/script2.php
@@ -1,30 +1,4 @@
-<?php /*
-$q = intval($_GET['q']);
-$con = mysqli_connect('hackohio2017.cligzm8ucbhx.us-east-1.rds.amazonaws.com:3306','dbmaster','welcometomydb','hackohio2017');
-if (!$con) {
-    die('Could not connect: ' . mysqli_error($con));
-}
-
-$sql="SELECT * FROM CLASS WHERE DepartmentID = 35";
-$result = mysqli_query($con,$sql);
-$resultData[] = array();
-
-while($row = $result->fetch_assoc())
-{
-    $rows[] = $row;
-}
-echo '<table>';
-foreach($rows as $value)
-{
-    echo '<tr>
-             <td>'.$value['ID'].'</td>
-             <td>'.$value['Number'].'</td>
-           </tr>';
-}           
-echo '</table>';
-mysqli_close($con);
-?>
-*/
+<?php
 $q = intval($_GET['q']);
 $con = mysqli_connect('hackohio2017.cligzm8ucbhx.us-east-1.rds.amazonaws.com:3306','dbmaster','welcometomydb','hackohio2017');
 if (!$con) {
