2e0eb05f9844fc14dce1afc244b2b4bbb37c0736
Jeremy Sandrof <jeremysandrof@Jeremys-MacBook-Pro-2.local>
2017-10-22 03:19:07 -0400

    added addClassToSemester and removeCourse, also added php file to query a single course from database

--- Web/script.js
+++ Web/script.js
@@ -41,3 +41,53 @@ function startUp() {
         xmlhttp.open("GET","startUp.php",true);
         xmlhttp.send();
 }
+
+function addClassToSemester(semester, courseID) {
+
+    var newDiv = document.createElement("div");
+    newDiv.setAttribute("id", courseID);
+    
+
+    if (window.XMLHttpRequest) {
+        // code for IE7+, Firefox, Chrome, Opera, Safari
+        xmlhttp = new XMLHttpRequest();
+    } else {
+        // code for IE6, IE5
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xmlhttp.onreadystatechange = function() {
+        
+        if (this.readyState == 4 && this.status == 200) {
+
+            newDiv.innerHTML = this.responseText;
+            document.getElementById("currentClasses").append(newDiv);
+
+            
+
+        }
+    };
+    xmlhttp.open("GET","queryOneClass.php?q="+courseID,true);
+    xmlhttp.send();
+    
+    var parsedCourseName = "FUCKIG SHIT TITTIES"; //retrieved using courseID
+    document.getElementById("sem" + semester).children[0].innerHTML += "<div id = '" + courseID + "'><p>" + parsedCourseName + "</p><button type='button' onclick='removeCourse(" + semester + "," + courseID + ")'>remove</button></div>";
+    
+}
+
+function removeCourse(semester, courseID) {
+    var children = document.getElementById("sem" + semester).children[0].children;
+    var index;
+    for (index=0; index < children.length; ++index) {
+
+        if (children[index].getAttribute("id") == courseID) {
+            children[index].parentNode.removeChild(children[index]);
+        
+        }
+    }
+
+    
+}
+
+function updateProgress() {
+
+}
